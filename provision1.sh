2aa4fdf674dc761b27d7c231ffa7566aaf84620d
boleynBOLEYNKENKEM <boleynkenkem@yahoo.fr>
2019-11-21 14:40:30 +0100

    Initial commit: Vagrant file & provisioning

---
+++ provision1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -u
+
+HOSTNAME="$(hostname)"
+
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$HOSTNAME" = "control" ]; then
+
+	ansible-playbook  -i inventory DNSmasq.yml
+	ansible-playbook  -i inventory maria.yml
+	
+
+if 
